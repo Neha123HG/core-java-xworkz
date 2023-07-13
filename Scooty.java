edb60121e7631408b595e09260b8a418f6361706
unknown <nehahg020@gmail.com>
2023-07-13 13:53:03 +0530

    adding a chains sandal scooty and waterFilter files crud

---
+++ Scooty.java
@@ -0,0 +1,18 @@
+class Scooty{
+
+static String sName="Active 5G";
+static float price="480000.00";
+static String enginType="SI engine";
+static String maxPower="7.96ps@7500rpm";
+static String frontByke="Drum";
+public static void main(String byke[])
+{
+System.out.println("sName is"+sName);
+System.out.println("price is"+ price);
+System.out.println("engintype is"+enginType );
+System.out.println("maxPower is"+ maxPower);
+System.out.println("frontByke is "+ frontByke);
+
+}
+
+}
