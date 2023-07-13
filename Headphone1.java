48040b8555ea776d09efa1f9e224409c029143f2
unknown <nehahg020@gmail.com>
2023-07-13 13:38:00 +0530

    message

---
+++ Headphone1.java
@@ -0,0 +1,20 @@
+class HeadPhone1
+{
+ static String brand="boat";
+ static int price=5000;
+ static int frequency =120;
+// String booleanisWireless="true"
+//static String battery="good"
+
+public static void main(String head[]){
+    boolean  isWireless=true;
+	String battery="good";
+	System.out.println("headphone brand is"+ brand);
+	System.out.println("headphone price is"+ price);
+	System.out.println("headphone frequency is"+ frequency);
+	System.out.println("headphone is wireless "+ isWireless);
+	System.out.println("headphone battery is "+ battery);
+
+}
+
+}
