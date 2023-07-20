fc5c48bd5b726c3ec0f0765a0abe85f93a6c69fe
Neha123HG <139327474+Neha123HG@users.noreply.github.com>
2023-07-20 08:04:32 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main{
+
+
+static int age[]={1,2,3,5,9};
+
+public static void main(String args[]){
+
+System.out.println("using loop");
+for( int i=0;i<age.length;i++){
+System.out.println(age[i]);
+}
+}
+}
